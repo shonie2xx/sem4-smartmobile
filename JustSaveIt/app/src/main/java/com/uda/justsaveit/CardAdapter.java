0578788c86dfece78a23172e6335840a87bf82a4
Ufuk Angay <u.angay@gmail.com>
2021-04-02 11:10:49 +0200

    Added Leaderboard Profile

---
+++ JustSaveIt/app/src/main/java/com/uda/justsaveit/CardAdapter.java
@@ -0,0 +1,42 @@
+package com.uda.justsaveit;
+
+//public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ItemViewHolder>{
+//
+//    private List<String> cardList;
+//
+//    public CardAdapter(List<String> list){
+//        cardList = list;
+//    }
+//
+//    //
+//    @NonNull
+//    @Override
+//    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.activity_leaderboard_profile, viewGroup, false);
+//        return new ItemViewHolder(view);
+//    }
+//
+//    // Here the Logic of the Code
+//    @Override
+//    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+//        if (i==0){
+//            itemViewHolder.container.setBackgroundRes
+//        }
+//    }
+//
+//    //Return size of the List of Strings
+//    @Override
+//    public int getItemCount() {
+//        return cardList.size();
+//    }
+//
+//    class ItemViewHolder extends RecyclerView.ViewHolder{
+//        RelativeLayout container;
+//        View divider;
+//        public ItemViewHolder(View itemView){
+//            super(itemView);
+//            container = itemView.findViewById(R.id.container);
+//            // divider = itemView.findViewById(R.id.);
+//        }
+//    }
+//}
--- JustSaveIt/app/src/main/java/com/uda/justsaveit/MainActivity.kt
+++ JustSaveIt/app/src/main/java/com/uda/justsaveit/MainActivity.kt
@@ -2,10 +2,15 @@ package com.uda.justsaveit
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Testing RealTime DB
+        var database = FirebaseDatabase.getInstance().reference
+        database.setValue("Data Saved in db")
     }
 }
