d9fd9a5bc811d48adf01d7e0bb5fd2c3a0b1c4d2
Daniel Vaswani <danielvaswani@Daniels-Mac.local>
2021-04-17 21:50:30 +0700

    testing code

--- JustSaveItProductionApp/app/src/main/java/com/example/justsaveit_newapp/firestore/FireStoreClass.kt
+++ JustSaveItProductionApp/app/src/main/java/com/example/justsaveit_newapp/firestore/FireStoreClass.kt
@@ -1,5 +1,6 @@
 package com.example.justsaveit_newapp.firestore
 
+import com.example.justsaveit_newapp.models.DateMap
 import com.example.justsaveit_newapp.models.MonthlyBudget
 import com.example.justsaveit_newapp.models.User
 import com.example.justsaveit_newapp.ui.AddIncomeActivity
@@ -7,6 +8,8 @@ import com.example.justsaveit_newapp.ui.HomePageActivity
 import com.example.justsaveit_newapp.ui.RegisterActivity
 import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.*
+import java.time.YearMonth
+import java.util.*
 
 class FireStoreClass {
 
@@ -37,9 +40,71 @@ class FireStoreClass {
                 }
     }
 
-    fun getLastMonthlyBudgetDate(userId: String){
+    fun getLastMonthlyBudgetDate(userId: String) : YearMonth?{
 //        get last added monthly budget so the new one can be added as new MonthlyBudget ( YearMonth. minusMonths(-1)
-    }
+        //get list of document snapshots, binary search the highest document.monthYear,
+        // return document.monthyear as a YearMonth format
+
+//        var date: Date? = null
+//        var dateMaps : ArrayList<DateMap>? = null
+//        val docRef = mFireStore.collection("users")
+//            .document(userId).collection("monthlybudgets").get()
+//
+//
+//        docRef.addOnSuccessListener {
+//            it.documents.forEach { doc ->
+//                doc.data
+//                doc.data!!.forEach {
+//                    if(it.key == "date"){
+//                        val map = it.value
+//                    }
+//                }
+//                val dm = it.value.toObject(DateMap::class.java)
+//                if (dm != null ) {
+//                    if()
+//                    dateMaps?.add(dm)
+//                }
+//
+//                //it.toObject(MonthlyBudget::class.java)
+////                val dateMapUnit  = it.getData()?.forEach {
+////                    if(it.key == "date") {
+////                        it.value.
+////                        var maps = MutableList<Map<String,Any>>()
+////                        maps.add(it.value)
+////                    }
+////                }
+//                }
+//                if(dateMaps!= null){
+//                    var highestYear = dateMaps[0].year!!.toInt()
+//                    var currentYear :Int
+//                    for (i in 0 until dateMaps!!.size-1) {
+//                        currentYear = dateMaps[i].year!!.toInt()
+//                            if(currentYear > highestYear){
+//                                highestYear = currentYear
+//                            }
+//                    }
+//                    //highest year found
+//                    var highestMonth = dateMaps[0].monthValue!!.toInt()
+//                    for (i in 0 until dateMaps!!.size-1) {
+//                        currentYear = dateMaps[i].year!!.toInt()
+//                        if()
+//                    }
+//                }
+//            }
+        return null
+        }
+            //.orderBy("date", Query.Direction.DESCENDING).limit(1).get()
+
+      /*      sortedQuery.addOnSuccessListener {
+                //QuerySnapshot to Documents, get the first doc
+                val doc = it.documents[0]
+                date = doc.getDate("date")
+            }
+                sortedQuery.addOnFailureListener {
+            }
+        return date*/
+// citiesRef.orderBy("name", Query.Direction.DESCENDING).limit(3)
+
 
     //if there is budgets USE
     fun editMonthlyBudget(activity: AddIncomeActivity, userId:String, monthYear: String,incomeAmount:Double,savings: Double){
