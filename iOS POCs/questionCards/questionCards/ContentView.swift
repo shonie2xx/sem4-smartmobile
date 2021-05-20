e70834ab84821fe7ddd6d48417ef65c4bd71cedd
Daniel Vaswani <d.vaswani@student.fontys.nl>
2021-05-20 23:28:20 +0700

    add edit profile page

--- iOS POCs/questionCards/questionCards/ContentView.swift
+++ iOS POCs/questionCards/questionCards/ContentView.swift
@@ -8,17 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var experience = ""
+    @State private var info = ""
+    @State private var email = ""
     var body: some View {
         VStack(alignment: .trailing){
             QuestionsView(question : "The quick brown fox jumps over the lazy dog")
             QuestionsView(question : "The quick brown fox jumps over the lazy dog")
             QuestionsView(question: "The quick brown fox jumps over the lazy dogThe quick brown fox jumps over the lazy dogThe quick brown fox jumps over the lazy dogThe quick brown fox jumps over the lazy dog")
         }
+        
+        NavigationView{
+            VStack {
+                
+                //Image(systemName: "pencil")
+                
+                Form{
+                    Section(header: Text("Personal Details")){
+                        TextField("Full name", text : $name)
+                        TextField("Business title", text : $experience)
+                        
+                        
+                        TextField("Contact email", text : $email)
+                        
+                    }
+                    
+                    Section(header : Text("About you")){
+                        TextField("", text: $info)
+                            .lineLimit(4)
+                                        .multilineTextAlignment(.leading)
+                                        .frame(minWidth: 100, maxWidth: 200, minHeight: 100, maxHeight: .infinity, alignment: .topLeading)
+                        
+                    }
+                    
+                }.navigationTitle("My details").navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button("Cancel") {
+                            print("Help tapped!")
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                            Text("Done")
+                        })
+                    }
+                }
+                
+            }
+            
+            
+        }
     }
 }
 
 struct QuestionsView : View {
     var question : String
+    
     var body : some View {
         ZStack{
             RoundedRectangle(cornerRadius: 10 , style:.continuous)
@@ -32,7 +79,7 @@ struct QuestionsView : View {
                 }
                 VStack{
                     HStack{
-                        Image(systemName : "pencil")
+                        Image(systemName: "pencil")
                             .renderingMode(.original)
                             .resizable(capInsets: EdgeInsets(top: 7.0, leading: 6.0, bottom: 8.0, trailing: 9.0), resizingMode: .tile)
                             .aspectRatio(contentMode: .fit)
@@ -47,6 +94,7 @@ struct QuestionsView : View {
                         Spacer()
                         VStack{
                             Text("23 comments")
+                                
                             Text("23000 Watching")
                             Text("Watch")
                                 .padding(10)
@@ -62,9 +110,11 @@ struct QuestionsView : View {
                             .shadow(radius: 1)
                         Text(question)
                     }
+                   
+                        Text("View answers - 10")
+                            .foregroundColor(Color.blue)
                     
-                    Text("View answers - 10")
-                        .foregroundColor(Color.blue)
+                    
                         
                 }
                 
