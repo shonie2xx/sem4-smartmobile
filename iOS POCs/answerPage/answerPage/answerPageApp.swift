53f1c9299bf24c5445892cc74bd6aa0b6ec920e5
Daniel Vaswani <d.vaswani@student.fontys.nl>
2021-05-21 18:04:56 +0700

    added more pages

---
+++ iOS POCs/answerPage/answerPage/answerPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  answerPageApp.swift
+//  answerPage
+//
+//  Created by Daniel Vaswani on 20/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct answerPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
